6a1ca6d98733f3702e651c21c972f6f39f64d4ef
brockp <brock.puente@gmail.com>
2020-10-13 18:13:41 -0700

    Game board testing

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -2,8 +2,13 @@
 public class GameBoard {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(" ");
+		
+				// random comment
+	}
+	
+	public GameBoard() {
+		
 	}
 
 }
