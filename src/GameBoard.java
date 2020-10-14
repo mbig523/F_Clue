7530ca0c659f8888b59684023a4f527db63e3357
brockp <brock.puente@gmail.com>
2020-10-13 20:08:01 -0700

    Sample Board
    Just messing around trying to find the best way to make the game board

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,14 +1,59 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class GameBoard {
-
-	public static void main(String[] args) {
-		System.out.println(" ");
+	
+	JButton[] tileButtons = new JButton[325];
+	
+	
+	public void CreateBoard() {
 		
-				// random comment
+		JFrame gameFrame = new JFrame();
+		gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gameFrame.setSize(800,600);
+		gameFrame.add(TileButtons());
+		gameFrame.setVisible(true);
+		
 	}
 	
-	public GameBoard() {
+	public JPanel TileButtons() {
+		JPanel buttonPanel = new JPanel(new GridLayout(18,18));
+		int kitchen[] = new int[] {0,1,2,3,4,18,19,20,21,22,36,37,38,39,40};
+		
+		for(int i = 0; i < 324; i++) {
+			tileButtons[i] = new JButton();
+			tileButtons[i].addActionListener(new ButtonListener(i));
+			tileButtons[i].setText(" " +i);
+			
+			
+			for(int j = 0; j < kitchen.length; j++) {
+				tileButtons[kitchen[j]].setBackground(Color.BLUE);
+				
+			}
+			
+			buttonPanel.add(tileButtons[i]);
+		}
+		
+		return buttonPanel;
+	}
+	
+	
+public class ButtonListener implements ActionListener {
+
+	private int i;
+	public ButtonListener(int i) {
+		this.i = i;
+	}
+		
+	public void actionPerformed(ActionEvent e) {
+		tileButtons[i].setBackground(Color.BLACK);
+			
+	}
 		
 	}
-
+	
+	
 }
+
+// WIP
--- src/RunGame.java
+++ src/RunGame.java
@@ -1,4 +1,23 @@
+// this class will create objects of every component of the game and run the game
+
+/* objects to create:
+	 * 
+	 * game board
+	 * cards
+	 * characters
+	 * dice
+	 * interfaces (detective card etc)
+	 * 
+	 * 
+	*/
 
 public class RunGame {
-
+			
+	public static void main(String args[]) {
+		GameBoard game = new GameBoard();
+		game.CreateBoard();
+	
+	}
+	
+	
 }
